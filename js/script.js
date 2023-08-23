9ffee8490295c0c1c2441ed4c2324d2343f7c6da
daida <138858179+daidasan@users.noreply.github.com>
2023-08-24 07:17:20 +0900

    nav+section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+$(".openbtn").click(function () { //ボタンがクリックされたら
+    $(this).toggleClass('active'); //ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive'); //ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive'); //丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () { //ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active'); //ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive'); //ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive'); //丸背景のcircleactiveクラスを除去
+});
+
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
